8a32cd584cb48742caed9ec9129d32201d78e034
Simon Harloff <simon.harloff@gmail.com>
2022-02-05 18:39:33 +1100

    Remove unncessary dependencies for CCS811 and DHT sensors
    Rename credentials to config file.

---
+++ src/config.h.tpl
@@ -0,0 +1,12 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define WIFI_SSID ""
+#define WIFI_PASSWORD ""
+
+#define MQTT_SERVER ""
+#define MQTT_PORT 1883
+#define MQTT_USER ""
+#define MQTT_PASSWORD ""
+
+#endif
--- src/esp32-env-sensor.ino
+++ src/esp32-env-sensor.ino
@@ -1,4 +1,4 @@
-#include "credentials.h"
+#include "config.h"
 
 #include <Wire.h>
 #include <SPI.h>
@@ -6,8 +6,19 @@
 #include <PubSubClient.h>
 #include <U8g2lib.h>
 #include <Adafruit_Sensor.h>
-#include <Adafruit_BME680.h>
-#include <Adafruit_CCS811.h>
+#include <bsec.h>
+
+struct BME680Result {
+  bool hasValues = false;
+  float temperature;
+  float humidity;
+  float pressure;
+  float gasResistance;
+  float iaq;
+  int iaqAccuracy;
+  float co2;
+  float voc;
+};
 
 // OLED display setup
 #define OLED_MOSI 23
@@ -18,39 +29,17 @@
 U8G2_SH1106_128X64_NONAME_1_4W_SW_SPI disp(U8G2_R0, OLED_CLK, OLED_MOSI, OLED_CS, OLED_DC, OLED_RST);
 
 // BME680 sensor
-Adafruit_BME680 bme;
-
-// CCS811 sensor
-Adafruit_CCS811 ccs;
+Bsec iaqSensor;
+BME680Result lastBmeResult;
+int bmeSensorStatus = 47788;
 
 // sensor oled display vars
-const char defaultTemperatureLine[]   = "   T: --.-C";
-const char defaultHumidityLine[]      = "   H: --.-%";
-const char defaultPressureLine[]      = "   P: ---.-hPa";
-const char defaultCO2Line[]           = " CO2: ---.-ppm";
-const char defaultTVOCLine[]          = "TVOC: ---.-ppb";
-const char temperatureLineTemplate[]  = "   T: %2.1fC";
-const char humidityLineTemplate[]     = "   H: %2.1f%%";
-const char pressureLineTemplate[]     = "   P: %3.1fhPa";
-const char co2LineTemplate[]          = " CO2: %3.1fppm";
-const char tvocLineTemplate[]         = "TVOC: %3.1fppb";
-
-struct BME680Result {
-  bool hasValues = false;
-  float temperature;
-  float humidity;
-  float pressure;
-};
-
-struct CCS811Result {
-  bool hasValues = false;
-  float co2;
-  float tvoc;
-};
+const char defaultSensorReadingLine[]   = "---.--";
+const char sensorReadingTemplate[]  = "%3.2f";
 
 // wifi vars
 unsigned long wifiConnectAttemptTime = 0;
-int wifiStatus = 42;
+int wifiStatus = 57879;
 
 // mqtt vars
 WiFiClient espClient;
@@ -58,21 +47,38 @@ PubSubClient mqttClient(espClient);
 const char mqttId[] = "esp32_aqi_sensor";
 const unsigned int mqttSendInterval = 5000;
 unsigned long lastMqttSendTime = 0;
+int mqttConnectionStatus = 32;
 
 void setup() {
   Serial.begin(115200);
   Serial.println("[setup] begin");
 
-  // intialize the CCS811 sensor
-  auto ccsResult = ccs.begin(0x5A);
-  if (!ccsResult) {
-    Serial.println("[setup] CCS811 error");
-  }
+  // initialize I2C on default pins
+  Wire.begin();
 
   // initialise BME680 sensor
-  auto bmeResult = bme.begin(0x76);
-  if (!bmeResult) {
-    Serial.println("[setup] BME680 error");
+  iaqSensor.begin(BME680_I2C_ADDR_PRIMARY, Wire);
+  auto iaqSensorOK = checkBME680Status();
+  if (iaqSensorOK) {
+    bsec_virtual_sensor_t sensorList[10] = {
+      BSEC_OUTPUT_RAW_TEMPERATURE,
+      BSEC_OUTPUT_RAW_PRESSURE,
+      BSEC_OUTPUT_RAW_HUMIDITY,
+      BSEC_OUTPUT_RAW_GAS,
+      BSEC_OUTPUT_IAQ,
+      BSEC_OUTPUT_STATIC_IAQ,
+      BSEC_OUTPUT_CO2_EQUIVALENT,
+      BSEC_OUTPUT_BREATH_VOC_EQUIVALENT,
+      BSEC_OUTPUT_SENSOR_HEAT_COMPENSATED_TEMPERATURE,
+      BSEC_OUTPUT_SENSOR_HEAT_COMPENSATED_HUMIDITY,
+    };
+
+    iaqSensor.updateSubscription(sensorList, 10, BSEC_SAMPLE_RATE_LP);
+    if (!checkBME680Status()) {
+      Serial.println("[setup] BME680 configuration error");
+    }
+  } else {
+    Serial.println("[setup] BME680 initialization error");
   }
 
   // initialize OLED display
@@ -83,13 +89,6 @@ void setup() {
 
   // initialize MQTT connection
   mqttClient.setServer(MQTT_SERVER, MQTT_PORT);
-
-  // intialize the BME sensor
-  bme.setTemperatureOversampling(BME680_OS_8X);
-  bme.setHumidityOversampling(BME680_OS_2X);
-  bme.setPressureOversampling(BME680_OS_4X);
-  bme.setIIRFilterSize(BME680_FILTER_SIZE_3);
-  bme.setGasHeater(320, 150); // 320*C for 150 ms
 
   Serial.println("[setup] done");
 }
@@ -106,19 +105,84 @@ void loop() {
   }
 
   auto bmeResult = readBME680();
-  auto ccsResult = readCCS811();
-  drawStatistics(bmeResult, ccsResult);
-
+  drawStatistics(bmeResult, lastBmeResult);
+  if (bmeResult.hasValues) {
+    lastBmeResult = bmeResult;
+  }
+  
   // send results via MQTT if the connection is established
   if (mqttConnected) {
-    sendMQTT(bmeResult, ccsResult);
+    sendMQTT(bmeResult);
   }
 
   delay(1000);
 }
 
-void sendMQTT(BME680Result bmeResult, CCS811Result ccsResult) {
-  
+void drawStatistics(BME680Result currentBmeResult, BME680Result previousBmeResult) {
+  BME680Result bmeResult = currentBmeResult;
+  if (!currentBmeResult.hasValues) {
+    bmeResult = previousBmeResult;
+  }
+
+  disp.firstPage();
+  do {
+    
+    // draw the grid that will hold our sensor readings
+    disp.drawVLine(40, 13, 116);
+    disp.drawVLine(85, 13, 116);
+    disp.drawHLine(0, 13, 128);
+    disp.drawHLine(0, 39, 128);
+
+    // draw status glyps for Wifi and BME sensor status
+    disp.setFont(u8g2_font_siji_t_6x10);
+    disp.drawGlyph(116, 12, wifiStatus);
+    disp.drawGlyph(104, 12, mqttConnectionStatus);
+    disp.drawGlyph(92, 12, bmeSensorStatus);
+
+    // draw the headings in each cell of the grid for the various readings
+    disp.setFont(u8g2_font_squeezed_r6_tr);
+    disp.drawStr(0, 23, "TMP (C)");
+    disp.drawStr(0, 48, "HUM (%)");
+    disp.drawStr(43, 23, "PRS (hPa)");
+    disp.drawStr(43, 48, "IAQ");
+    disp.drawStr(88, 23, "CO2 (PPM)");
+    disp.drawStr(88, 48, "VOC (PPM)");
+
+    if (bmeResult.hasValues) {
+      char output[sizeof(defaultSensorReadingLine)];
+      disp.setFont(u8g2_font_helvB08_tf);
+
+      snprintf(output, sizeof(output), sensorReadingTemplate, bmeResult.temperature);
+      disp.drawStr(0, 34, output); // temperature
+
+      snprintf(output, sizeof(output), sensorReadingTemplate, bmeResult.humidity);
+      disp.drawStr(0, 59, output); // humidity
+
+      snprintf(output, sizeof(output), sensorReadingTemplate, bmeResult.pressure);
+      disp.drawStr(43, 34, output); // pressure
+
+      snprintf(output, sizeof(output), sensorReadingTemplate, bmeResult.iaq);
+      disp.drawStr(43, 59, output); // IAQ
+
+      snprintf(output, sizeof(output), sensorReadingTemplate, bmeResult.co2);
+      disp.drawStr(88, 34, output); // CO2
+
+      snprintf(output, sizeof(output), sensorReadingTemplate, bmeResult.voc);
+      disp.drawStr(88, 59, output); // VOC
+    } else {
+      disp.setFont(u8g2_font_helvB08_tf);
+      disp.drawStr(0, 34, defaultSensorReadingLine); // temperature
+      disp.drawStr(0, 59, defaultSensorReadingLine); // humidity
+      disp.drawStr(43, 34, defaultSensorReadingLine); // pressure
+      disp.drawStr(43, 59, defaultSensorReadingLine); // IAQ
+      disp.drawStr(88, 34, defaultSensorReadingLine); // CO2
+      disp.drawStr(88, 59, defaultSensorReadingLine); // VOC
+    }
+  } 
+  while ( disp.nextPage() );
+}
+
+void sendMQTT(BME680Result bmeResult) {
   // if we are not connected to MQTT broker then return
   if (!mqttClient.connected()) return;
 
@@ -128,33 +192,33 @@ void sendMQTT(BME680Result bmeResult, CCS811Result ccsResult) {
   if (!bmeResult.hasValues) return;
 
   if (lastMqttSendTime == 0 || (lastMqttSendTime + mqttSendInterval) < millis()) {
-    Serial.println("[runMQTT] Send MQTT reading.");
+    // Serial.println("[runMQTT] Send MQTT reading.");
 
     if (bmeResult.hasValues) {
-      char temperatureString[5];
-      snprintf(temperatureString, 5, "%2.1f", bmeResult.temperature);     
-      
-      char humdityString[5];
-      snprintf(humdityString, 5, "%2.1f", bmeResult.humidity);
+      char temperatureString[7];
+      char humdityString[7];
+      char pressureString[7];
+      char co2String[7];
+      char vocString[7];
+      char iaqString[6];
+      char iaqAccuracyString[2];
 
-      char pressureString[5];
-      snprintf(pressureString, 5, "%3.1f", bmeResult.pressure);
+      snprintf(temperatureString, sizeof(temperatureString), "%3.2f", bmeResult.temperature);     
+      snprintf(humdityString, sizeof(humdityString), "%3.2f", bmeResult.humidity);
+      snprintf(pressureString, sizeof(pressureString), "%3.2f", bmeResult.pressure);
+      snprintf(co2String, sizeof(co2String), "%3.2f", bmeResult.co2);
+      snprintf(vocString, sizeof(vocString), "%3.2f", bmeResult.voc);
+      snprintf(iaqString, sizeof(iaqString), "%2.2f", bmeResult.iaq);
+      snprintf(iaqAccuracyString, sizeof(iaqAccuracyString), "%d", bmeResult.iaqAccuracy);
       
       mqttClient.publish("temperature", temperatureString);
       mqttClient.publish("humidity", humdityString);
       mqttClient.publish("pressure", pressureString);
-    }
-
-    if (ccsResult.hasValues) {
-      char co2String[7];
-      snprintf(co2String, 7, "%3.1f", ccsResult.co2);
-      
-      char tvocString[7];
-      snprintf(tvocString, 7, "%3.1f", ccsResult.tvoc);
-
       mqttClient.publish("co2", co2String);
-      mqttClient.publish("tvoc", tvocString);
-    } 
+      mqttClient.publish("voc", vocString);
+      mqttClient.publish("iaq", iaqString);
+      mqttClient.publish("iaq_accuracy", iaqAccuracyString);
+    }
 
     lastMqttSendTime = millis();
   }
@@ -162,11 +226,12 @@ void sendMQTT(BME680Result bmeResult, CCS811Result ccsResult) {
 
 bool setupMQTT() {
   if ( !mqttClient.connected() ) {
-    Serial.println("[setupMQTT] MQTT client NOT connected.");
+    mqttConnectionStatus = 32;
     mqttClient.connect(mqttId, MQTT_USER, MQTT_PASSWORD);
     return false;
   }
 
+  mqttConnectionStatus = 57965;
   return true;
 }
 
@@ -174,95 +239,65 @@ bool setupWiFi() {
   uint8_t status = WiFi.status();
   
   if (status == WL_CONNECTED) {
-    wifiStatus = 113;
+    wifiStatus = 57882;
     return true;
   } else if (status == WL_DISCONNECTED && (wifiConnectAttemptTime == 0 || millis() > wifiConnectAttemptTime + 30000)) {
-    wifiStatus = 108;
+    wifiStatus = 57631;
     WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
     wifiConnectAttemptTime = millis();
   } else if (status == WL_CONNECT_FAILED) {
-    wifiStatus = 33;
+    wifiStatus = 57463;
   }
 
   return false;
 }
 
-void drawStatistics(BME680Result bmeResult, CCS811Result ccsResult) {
-  disp.firstPage();
-  do {
-    disp.setFont(u8g2_font_8x13B_tf);
-
-    char temperatureLine[sizeof(defaultTemperatureLine)];
-    char humidityLine[sizeof(defaultHumidityLine)];
-    char co2Line[sizeof(defaultCO2Line)];
-    char tvocLine[sizeof(defaultTVOCLine)];
+bool checkBME680Status() {
+  if (iaqSensor.status != BSEC_OK) {
+    if (iaqSensor.status < BSEC_OK) {
+      Serial.println("BSEC error code : " + String(iaqSensor.status));
+    } else {
+      Serial.println("BSEC warning code : " + String(iaqSensor.status));
+    }
     
-    if (bmeResult.hasValues) {
-      snprintf(temperatureLine, sizeof(temperatureLine), temperatureLineTemplate, bmeResult.temperature);
-      snprintf(humidityLine, sizeof(humidityLine), humidityLineTemplate, bmeResult.humidity);
+    return false;
+  }
+
+  if (iaqSensor.bme680Status != BME680_OK) {
+    if (iaqSensor.bme680Status < BME680_OK) {
+      Serial.println("BME680 error code : " + String(iaqSensor.bme680Status));
     } else {
-      strncpy(temperatureLine, defaultTemperatureLine, sizeof(temperatureLine));
-      strncpy(humidityLine, defaultHumidityLine, sizeof(humidityLine));
+      Serial.println("BME680 warning code : " + String(iaqSensor.bme680Status));
     }
 
-    if (ccsResult.hasValues) {
-      snprintf(co2Line, sizeof(co2Line), co2LineTemplate, ccsResult.co2);
-      snprintf(tvocLine, sizeof(tvocLine), tvocLineTemplate, ccsResult.tvoc);
-    } else {
-      strncpy(co2Line, defaultCO2Line, sizeof(co2Line));
-      strncpy(tvocLine, defaultTVOCLine, sizeof(tvocLine));
-    }
+    return false;
+  }
 
-    disp.drawStr(0, 12, temperatureLine);
-    disp.drawStr(0, 28, humidityLine);
-    disp.drawStr(0, 44, co2Line);
-    disp.drawStr(0, 60, tvocLine);
-    disp.setFont(u8g2_font_iconquadpix_m_all);
-    disp.drawGlyph(112, 12, wifiStatus);
-  } 
-  while ( disp.nextPage() );
+  return true;
 }
 
 BME680Result readBME680() {
   BME680Result result;
 
-  unsigned long endTime = bme.beginReading();
-  if (endTime == 0) {
-    Serial.println("BME680 Error beginning reading");
-    return result;
+  if (iaqSensor.run()) { // If new data is available  
+    result.hasValues = true;  
+    result.temperature = iaqSensor.temperature;
+    result.humidity = iaqSensor.humidity;
+    result.pressure = iaqSensor.pressure / 100.0;
+    result.gasResistance = iaqSensor.gasResistance;
+    result.iaq = iaqSensor.iaq;
+    result.iaqAccuracy = iaqSensor.iaqAccuracy;
+    result.co2 = iaqSensor.co2Equivalent;
+    result.voc = iaqSensor.breathVocEquivalent;
+
+    if (iaqSensor.iaqAccuracy == 0) {
+      bmeSensorStatus = 57788;
+    } else if (iaqSensor.iaqAccuracy == 1) {
+      bmeSensorStatus = 57795;
+    } else {
+      bmeSensorStatus = 57794;
+    }
   }
 
-  if (!bme.endReading()) {
-    Serial.println("BME680 Error ending reading");
-    return result;
-  }
-
-  result.hasValues = true;
-  result.temperature = bme.temperature;
-  result.humidity = bme.humidity;
-  result.pressure = (bme.pressure / 100.0);
-
-  // Serial.printf("BME680 result - temp: %2.1f; humid: %2.1f; hPA: %3.1f\n", result.temperature, result.humidity, result.pressure);
-  
   return result;
 }
-
-CCS811Result readCCS811() {
-  CCS811Result result;
-  
-  // if the CCS is not available return
-  if (!ccs.available()) return result;
-
-  // if there is an error then return
-  if (ccs.readData() != 0) {
-    Serial.println("CCS811 Error");
-    return result;
-  }
-
-  result.hasValues = true;
-  result.co2 = ccs.geteCO2();
-  result.tvoc = ccs.getTVOC();
-
-  return result;
-} 
-
